605b9b6114b4d40ed661c365ba7efe71e4a0c880
Nakula <nakulamarvellio@gmail.com>
2021-04-06 23:11:26 +0700

    ADD: react-redux & redux-thunk

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
+    <Provider store={store}> 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -13,6 +18,7 @@ function App() {
       <hr/>
       <Posts />
     </div>
+    </Provider>
   );
 }
 
